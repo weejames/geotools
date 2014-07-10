f116aabb6a5d0d6a525baeb834f0e92ffe3a8dc3
James Constable <me@jamesconstable.co.uk>
2014-07-10 21:07:19 +0100

    Starting to put togethertest suite

---
+++ src/Geopoint.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace \WeeJames\Geotools;
+
+class Geopoint
+{
+
+    // coordinates of this point
+    private $latitude;
+    private $longitude;
+
+    // available units
+    public static GEO_UNIT_KM = 'km';
+    public static GEO_UNIT_MILES = 'miles';
+
+
+    // available algorithms
+    public static ALGORITHM_FLAT = 'flat';
+    public static ALGORITHM_HAVERSINE = 'haversine';
+
+    // stores the current operation taking place
+    public $currentOperation;
+
+    // current units
+    public $currentUnit = 'km';
+
+    // target point
+
+    public function __construct($latitude, $Longitude)
+    {
+        $this->latitude = $latitude;
+        $this->longitude = $longitude;
+    }
+
+
+    /**
+     * Sets the current operation to distance
+     */
+    public function distance()
+    {
+
+    }
+
+    public function in($unit)
+    {
+
+    }
+
+    public function to(Coordinate $target)
+    {
+
+    }
+
+    public function getLatitude()
+    {
+
+    }
+
+    public function getLongitude()
+    {
+
+    }
+}
---
+++ tests/GeopointTest.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace \WeeJames\Geotools\Test;
+
+class GeopointTest
+{
+    
+}

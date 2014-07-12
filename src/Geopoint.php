5a8b08117e1644e7d28a8e0d50df7163821bcf80
James Constable <me@jamesconstable.co.uk>
2014-07-12 11:12:32 +0100

    Implemented basic set up for target point, algorithm and units.

--- src/Geopoint.php
+++ src/Geopoint.php
@@ -4,30 +4,34 @@ namespace WeeJames\Geotools;
 
 class Geopoint
 {
+    // available units
+    const GEO_UNIT_KM = 'km';
+    const GEO_UNIT_MILES = 'miles';
+
+
+    // available algorithms
+    const ALGORITHM_FLAT = 'flat';
+    const ALGORITHM_HAVERSINE = 'haversine';
 
     // coordinates of this point
     private $latitude;
     private $longitude;
 
-    // available units
-    public static $GEO_UNIT_KM = 'km';
-    public static $GEO_UNIT_MILES = 'miles';
-
-
-    // available algorithms
-    public static $ALGORITHM_FLAT = 'flat';
-    public static $ALGORITHM_HAVERSINE = 'haversine';
-
     // stores the current operation taking place
     public $currentOperation;
 
     // current units
-    public $currentUnit = 'km';
+    public $currentUnit;
 
     // target point
+    public $targetGeopoint;
+
+    // current algorithm
+    public $currentAlgorithm;
 
     public function __construct($latitude, $longitude)
     {
+        $this->currentUnit = self::GEO_UNIT_KM;
         $this->latitude = $latitude;
         $this->longitude = $longitude;
     }
@@ -43,12 +47,25 @@ class Geopoint
 
     public function in($unit)
     {
-
+        $this->currentUnit = $unit;
+        return $this;
     }
 
-    public function to(Coordinate $target)
+    public function to(Geopoint $target)
     {
+        $this->targetGeopoint = $target;
+        return $this;
+    }
 
+    public function using($algorithm)
+    {
+        $this->currentAlgorithm = $algorithm;
+        return $this;
+    }
+
+    public function getTarget()
+    {
+        return $this->targetGeopoint;
     }
 
     public function getLatitude()
@@ -59,5 +76,15 @@ class Geopoint
     public function getLongitude()
     {
         return $this->longitude;
+    }
+
+    public function getUnit()
+    {
+        return $this->currentUnit;
+    }
+
+    public function getAlgorithm()
+    {
+        return $this->currentAlgorithm;
     }
 }
--- tests/GeopointTest.php
+++ tests/GeopointTest.php
@@ -5,17 +5,89 @@ namespace WeeJames\Geotools\Tests;
 class GeopointTest extends \PHPUnit_Framework_TestCase
 {
 
+    // Glasgow
+    private $startLatitude = 55.8580;
+    private $startLongitude = 4.2590;
+
+    // NewYork
+    private $targetLatitude = 40.7127;
+    private $targetLongitude = 74.0059;
+
+    public function setUp()
+    {
+
+    }
+
     /**
      * @test
      */
     public function geopointCanBeCreatedSuccessfully()
     {
-        $latitude = 55.8580;
-        $longitude = 4.2590;
+        $startPoint = new \WeeJames\Geotools\Geopoint(
+            $this->startLatitude,
+            $this->startLongitude
+        );
 
-        $point = new \WeeJames\Geotools\Geopoint($latitude, $longitude);
+        $this->assertEquals($this->startLatitude, $startPoint->getLatitude());
+        $this->assertEquals($this->startLongitude, $startPoint->getLongitude());
+    }
 
-        $this->assertEquals($latitude, $point->getLatitude());
-        $this->assertEquals($longitude, $point->getLongitude());
+    /**
+     * @test
+     */
+    public function unitCanBeSetSuccessfully()
+    {
+        $startPoint = new \WeeJames\Geotools\Geopoint(
+            $this->startLatitude,
+            $this->startLongitude
+        );
+
+        $startPoint->in(\WeeJames\Geotools\Geopoint::GEO_UNIT_KM);
+
+        $this->assertEquals(\WeeJames\Geotools\Geopoint::GEO_UNIT_KM, $startPoint->getUnit());
+
+        $startPoint->in(\WeeJames\Geotools\Geopoint::GEO_UNIT_MILES);
+
+        $this->assertEquals(\WeeJames\Geotools\Geopoint::GEO_UNIT_MILES, $startPoint->getUnit());
+
+    }
+
+    /**
+     * @test
+     */
+    public function targetGeopointCanBeSetSuccessfully()
+    {
+        $startPoint = new \WeeJames\Geotools\Geopoint(
+            $this->startLatitude,
+            $this->startLongitude
+        );
+
+        $targetPoint = new \WeeJames\Geotools\Geopoint(
+            $this->targetLatitude,
+            $this->targetLongitude
+        );
+
+        $startPoint->to($targetPoint);
+
+        $this->assertEquals($targetPoint, $startPoint->getTarget());
+    }
+
+    /**
+     * @test
+     */
+    public function algorithmCanBeSetSuccessfully()
+    {
+        $startPoint = new \WeeJames\Geotools\Geopoint(
+            $this->startLatitude,
+            $this->startLongitude
+        );
+
+        $startPoint->using(\WeeJames\Geotools\Geopoint::ALGORITHM_FLAT);
+
+        $this->assertEquals(\WeeJames\Geotools\Geopoint::ALGORITHM_FLAT, $startPoint->getAlgorithm());
+
+        $startPoint->using(\WeeJames\Geotools\Geopoint::ALGORITHM_HAVERSINE);
+
+        $this->assertEquals(\WeeJames\Geotools\Geopoint::ALGORITHM_HAVERSINE, $startPoint->getAlgorithm());
     }
 }
